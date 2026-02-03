726a792e386922f7ab0fef2f86094b72723c301e
Alfonso Emilio Gomez Selva <stein.hakase.vs@gmail.com>
2026-02-02 20:52:07 -0600

    Add sha256 checksum to formula

--- Formula/lumentui.rb
+++ Formula/lumentui.rb
@@ -2,6 +2,7 @@ class Lumentui < Formula
   desc "Elegant NestJS-based product monitoring system with real-time macOS notifications"
   homepage "https://github.com/victorstein/lumentui"
   url "https://registry.npmjs.org/lumentui/-/lumentui-1.0.0.tgz"
+  sha256 "5f5e10c9b27d392be9c205308af19bc471700e6c9ac1256f87bcc1177bad4927"
   license "MIT"
 
   depends_on "node"
