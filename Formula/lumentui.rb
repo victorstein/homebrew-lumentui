86a5e2aae82565bcafdde1821a9e3adcb716e3b1
Alfonso Emilio Gomez Selva <stein.hakase.vs@gmail.com>
2026-02-02 20:41:14 -0600

    Add lumentui formula

---
+++ Formula/lumentui.rb
@@ -0,0 +1,17 @@
+class Lumentui < Formula
+  desc "Elegant NestJS-based product monitoring system with real-time macOS notifications"
+  homepage "https://github.com/victorstein/lumentui"
+  url "https://registry.npmjs.org/lumentui/-/lumentui-1.0.0.tgz"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"lumentui", "--help"
+  end
+end
